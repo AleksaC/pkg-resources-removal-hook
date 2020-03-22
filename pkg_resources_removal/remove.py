5727d8f033d8eb6d95c26de992c2794aa71e8756
AleksaC <aleksacukovic1@gmail.com>
2020-03-23 00:29:25 +0100

    Add support for multiple requirements files

--- pkg_resources_removal/remove.py
+++ pkg_resources_removal/remove.py
@@ -15,39 +15,50 @@ REQUIREMENTS_FILENAMES = {
 }
 
 
-def get_requirements_file_path(files):
-    for file_path in files:
-        for file_name in REQUIREMENTS_FILENAMES:
-            if file_name in file_path and os.path.exists(file_path):
-                return file_path
-
-
-def main(argv=None):
+def parse_args(args):
     parser = argparse.ArgumentParser()
 
     parser.add_argument(
         "filenames", nargs="*", help="Filenames pre-commit believes are changed.",
     )
 
-    args = parser.parse_args(argv)
+    return parser.parse_args(args)
 
-    reqs_fname = get_requirements_file_path(args.filenames)
 
-    if reqs_fname:
-        try:
-            with open(reqs_fname, "r+") as f:
-                reqs = f.readlines()
-                reqs_filtered = list(
-                    filter(lambda req: "pkg-resources" not in req, reqs)
-                )
-                if len(reqs) != len(reqs_filtered):
-                    f.seek(0)
-                    f.writelines(reqs_filtered)
-                    f.truncate()
-                    os.system("git add %s" % reqs_fname)
-                    print("Removed pkg-resources from %s" % reqs_fname)
-        except IOError:
-            return 1
+def get_requirements_file_paths(files):
+    fpaths = []
+
+    for file_path in files:
+        for file_name in REQUIREMENTS_FILENAMES:
+            if file_name in file_path and os.path.exists(file_path):
+                fpaths.append(file_path)
+
+    return fpaths
+
+
+def update_reqs(reqs_fpath):
+    with open(reqs_fpath, "r+") as f:
+        reqs = f.readlines()
+        reqs_filtered = list(filter(lambda req: "pkg-resources" not in req, reqs))
+        if len(reqs) != len(reqs_filtered):
+            f.seek(0)
+            f.writelines(reqs_filtered)
+            f.truncate()
+            os.system("git add %s" % reqs_fpath)
+            print("Removed pkg-resources from %s" % reqs_fpath)
+
+
+def main(argv=None):
+    args = parse_args(argv)
+
+    reqs_fpaths = get_requirements_file_paths(args.filenames)
+
+    if reqs_fpaths:
+        for reqs_fpath in reqs_fpaths:
+            try:
+                update_reqs(reqs_fpath)
+            except IOError:
+                return 1
 
     return 0
 
