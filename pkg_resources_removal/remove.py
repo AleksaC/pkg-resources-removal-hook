4e95ec4401e4904caa7cb293dc53a7e565d2d42e
AleksaC <aleksacukovic1@gmail.com>
2020-04-12 01:08:18 +0200

    Make hook fail after performing side effects

--- pkg_resources_removal/remove.py
+++ pkg_resources_removal/remove.py
@@ -8,6 +8,8 @@ import subprocess
 import sys
 
 
+_updated = False
+
 REQUIREMENTS_FILENAMES = {
     "requirements.txt",
     "requirements-test.txt",
@@ -20,6 +22,11 @@ def parse_args(args):
 
     parser.add_argument(
         "filenames", nargs="*", help="Filenames pre-commit believes are changed.",
+    )
+    parser.add_argument(
+        "--auto-add",
+        help="Automatically stage changes made to files within the hook",
+        action="store_true",
     )
 
     return parser.parse_args(args)
@@ -36,16 +43,24 @@ def get_requirements_file_paths(files):
     return fpaths
 
 
-def update_reqs(reqs_fpath):
+def update_reqs(reqs_fpath, auto_add):
+    global _updated
+
     with open(reqs_fpath, "r+") as f:
         reqs = f.readlines()
         reqs_filtered = list(filter(lambda req: "pkg-resources" not in req, reqs))
+
         if len(reqs) != len(reqs_filtered):
             f.seek(0)
             f.writelines(reqs_filtered)
             f.truncate()
-            os.system("git add %s" % reqs_fpath)
+
             print("Removed pkg-resources from %s" % reqs_fpath)
+
+            if auto_add:
+                os.system("git add %s" % reqs_fpath)
+
+            _updated = True
 
 
 def main(argv=None):
@@ -56,9 +71,23 @@ def main(argv=None):
     if reqs_fpaths:
         for reqs_fpath in reqs_fpaths:
             try:
-                update_reqs(reqs_fpath)
+                update_reqs(reqs_fpath, args.auto_add)
             except IOError:
                 return 1
+
+    if _updated:
+        if args.auto_add:
+            print(
+                "\nFiles were modified and staged inside the hook, "
+                "please commit again!"
+            )
+        else:
+            print(
+                "\nFiles were modified inside the hook, "
+                "please add those files and commit again!"
+             )
+
+        return 1
 
     return 0
 
